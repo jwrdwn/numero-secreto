0579583ab891552253ce1e0026e3a61b337dbf21
jwrdwn <jordannogs@gmail.com>
2023-01-12 12:32:03 -0300

    Botão de jogar novamente

--- js/verificacao.js
+++ js/verificacao.js
@@ -9,6 +9,8 @@ function verificaAcerto(chute) {
             <h1 id="acertou">Você acertou!</h1>
             <h3>O número secreto era ${numeroSecreto}!</h3>
             <h1 id="smile">:)</h1>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
     } else if(chute > numeroSecreto) {
         dica.innerHTML = `<div>O número secreto é menor! <i class="fa-solid fa-circle-down"></i></div>`
@@ -16,3 +18,9 @@ function verificaAcerto(chute) {
         dica.innerHTML = `<div>O número secreto é maior! <i class="fa-solid fa-circle-up"></i></div>`
     }
 }
+
+document.body.addEventListener('click', evento => {
+    if(evento.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
